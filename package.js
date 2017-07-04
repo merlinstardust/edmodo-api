0f24ba00c28c5122969dd25c024367ce547d296a
Merlin Patterson <merlinpatt@gmail.com>
2017-07-04 10:21:17 -0400

    Bump version number

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'merlin:edmodo-api',
   summary: 'API service for Edmodo accounts',
   git: 'https://github.com/merlinpatt/edmodo-api',
-  version: '0.0.5',
+  version: '1.0.0',
   documentation: 'README.md',
 });
 
@@ -10,7 +10,7 @@ Package.onUse(function (api) {
   api.versionsFrom('1.3.2.2');
   api.use('ecmascript');
 
-  api.use('merlin:edmodo@1.0.7', ['client', 'server']);
+  api.use('merlin:edmodo@2.0.0', ['client', 'server']);
   api.use('merlin:external-api@0.0.3');
 
   api.addFiles('edmodo-api.js');
